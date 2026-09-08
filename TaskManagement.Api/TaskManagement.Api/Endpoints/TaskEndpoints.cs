584236d8add5fa14c4bb7fee938c40bd0af70785
EmreWolf58 <emrekurt14070@gmail.com>
2026-09-08 07:48:25 +0300

    feat: taskendpoints add mapPut, mapDelete, group

--- TaskManagement.Api/TaskManagement.Api/Endpoints/TaskEndpoints.cs
+++ TaskManagement.Api/TaskManagement.Api/Endpoints/TaskEndpoints.cs
@@ -6,19 +6,59 @@ namespace TaskManagement.Api.Endpoints
 {
     public static class TaskEndpoints
     {
-        public static void MapTaskEndpoints(this WebApplication app)
+        public static void MapTaskEndpoints(this IEndpointRouteBuilder app)
         {
-            app.MapGet("/minimal/tasks", ([AsParameters] TaskQuery query, ITaskService taskService) =>
+            var group = app.MapGroup("/minimal/tasks")
+                .WithGroupName("v1")
+                .WithTags("Minimal Tasks");
+
+            group.MapGet("/", ([AsParameters] TaskQuery query, ITaskService taskService) =>
             {
                 var result = taskService.GetAll(query);
                 return Results.Ok(result);
-            }).WithGroupName("v1");
+            });
 
-            app.MapPost("/minimal/tasks", async([FromBody] CreateTaskDto createTaskDto, ITaskService taskService, CancellationToken cancellationToken) =>
+            group.MapPost("/", async([FromBody] CreateTaskDto createTaskDto, ITaskService taskService, CancellationToken cancellationToken) =>
             {
                 var createdTask = await taskService.CreateAsync(createTaskDto, cancellationToken);
                 return Results.Created($"/minimal/tasks/{createdTask.Id}", createdTask);
-            }).WithGroupName("v1");
+            });
+
+            group.MapGet("/{id:int}", (int id, ITaskService taskService) =>
+            {
+                var task = taskService.GetById(id);
+
+                if (task is null)
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(task);
+            });
+
+            group.MapPut("/{id:int}", async (int id, UpdateTaskDto updateTaskDto, ITaskService taskService, CancellationToken cancellationToken) =>
+            {
+                var updateTask = await taskService.UpdateAsync(id, updateTaskDto, cancellationToken);
+
+                if (updateTask is null)
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(updateTask);
+            });
+
+            group.MapDelete("/{id:int}" , async (int id, ITaskService taskService, CancellationToken cancellationToken) =>
+            {
+                var deleted = await taskService.DeleteAsync(id, cancellationToken);
+
+                if (!deleted)
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.NoContent();
+            });
         }
     }
 }
