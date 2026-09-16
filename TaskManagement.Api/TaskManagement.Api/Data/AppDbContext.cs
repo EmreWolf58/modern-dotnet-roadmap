7b7a52c7d3daa70f88bbf99044a541dea0bf81d7
EmreWolf58 <emrekurt14070@gmail.com>
2026-09-16 08:23:08 +0300

    feat: data klasörü ve sql paketleri eklendi.

---
+++ TaskManagement.Api/TaskManagement.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Api.Model.TaskModel;
+
+namespace TaskManagement.Api.Data
+{
+    public class AppDbContext : DbContext // Bizim AppDbContext sınıfımız EF Core'un DbContext sınıfından miras alıyor.
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //DbContextOptions<AppDbContext>: AppDbContext için EF Core ayarlarını taşır.
+        {
+            //base(options): DbContextOptions context'in ayarlarını taşır, base(options) bu ayarları EF Core'un temel DbContext sınıfına aktarır.
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+
+    }
+}
