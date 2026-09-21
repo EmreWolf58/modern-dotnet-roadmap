b19060832bf792ff0a5821393e83b806830aa393
EmreWolf58 <emrekurt14070@gmail.com>
2026-09-21 08:25:23 +0300

    feat: add task priority migration

--- TaskManagement.Api/TaskManagement.Api/Model/TaskModel/TaskModel.cs
+++ TaskManagement.Api/TaskManagement.Api/Model/TaskModel/TaskModel.cs
@@ -9,5 +9,6 @@
         public DateTime CreatedDate { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime? DeletedDate { get; set; }
+        public int Priority { get; set; }
     }
 }
